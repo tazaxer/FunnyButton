175cc503c2ed6d6552c74ed689f49b94c4172ac4
Dennis Gyftakis <80547849+tazaxer@users.noreply.github.com>
2023-08-12 14:33:30 +0300

    Added Code

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const buttonHeight = 150;
+const buttonWidth = 250;
 
+const maxHeight = window.innerHeight - buttonHeight;
+const maxWidth = window.innerWidth - buttonWidth;
+
+window.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('button');
+
+    button.addEventListener('onmouseover', () => {
+        button.style.left = Math.floor(Math.random * maxWidth + 1) + 'px';
+        button.style.top = Math.floor(Math.random * maxHeight + 1) + 'px';
+
+    })
+})
